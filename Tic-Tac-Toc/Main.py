220668b1625e8a7bb6929fb0eae988bc5dff1f94
poinwater <10836861@qq.com>
2020-12-05 14:04:10 -0800

    Add function doc

--- Tic-Tac-Toc/Main.py
+++ Tic-Tac-Toc/Main.py
@@ -42,6 +42,7 @@ class tic_tac_toc(object):
             return -1, -1
 
     def AI_play(self):
+        # return tuple
         empty_cells = self.board.compute_empty_cells(self.board.board)
         new_board = self.board.board.copy()
         for row, column in empty_cells:
@@ -51,6 +52,7 @@ class tic_tac_toc(object):
             new_board[row][column] = self.user_mark
             if self.board.check_win(new_board, self.user_mark):
                 return (row, column)
+            # undo
             new_board[row][column] = 0
         return random.choice(empty_cells)
 
@@ -69,6 +71,7 @@ class tic_tac_toc(object):
             self.board.print_board()
             print("Turn for '{}'".format(self.board.transfer_cell(mark)))
             row, column = -1, -1
+            # Make movement
             if self.is_user_turn:
                 # Loop if input is invalid
                 while row == -1 and column == -1:
@@ -81,16 +84,19 @@ class tic_tac_toc(object):
                 row, column = self.AI_play()
             self.board.move(row, column, mark)
             self.is_user_turn = not self.is_user_turn
-
+            # Checking Game Status
             if self.board.check_win(self.board.board, mark):
+                # Win or Lose
                 self.board.print_board()
                 print('{} has won!!  YaaY'.format(self.board.transfer_cell(mark)))
             elif self.board.check_complete(self.board.board):
+                # Tie
                 self.board.print_board()
                 print('\nGame is Tie !! Play it again')
             else:
+                # Keep Playing
                 continue
-            
+            # Replay or not
             user_info = input("Start a new game? ('q' to quit): ")
             if user_info != 'q':
                 self.start()
